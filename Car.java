9a8205068d28f734f746145d997021bd3c40ec3f
Erik <erik32x@protonmail.com>
2023-11-07 17:48:42 +0100

    Open Closed principle implemented

---
+++ Car.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+
+public class Car {
+
+    public int nrDoors; // Number of doors on the car
+
+    public double enginePower; // Engine power of the car
+
+    public double currentSpeed; // The current speed of the car
+
+    public Color color; // Color of the car
+
+    public String modelName; // The car model name
+
+    public Car(int nrDoors, Color color, double enginePower, String modelName) {
+        this.nrDoors = nrDoors;
+        this.color = color;
+        this.enginePower = enginePower;
+        this.modelName = modelName;
+        stopEngine();
+    }
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+
+
+}
